21e9bb45be32f69b0ac9e0e25e587c9d775162c1
Musa Morena Marcusso Manhães <Musa.Marcusso@de.bosch.com>
2017-02-07 14:41:31 +0100

    Removing old OpenRAVE files for the Oberon arm
    Signed-off-by: Musa Morena Marcusso Manhães <Musa.Marcusso@de.bosch.com>

--- uuv_manipulators/oberon/oberon_description/openrave/joints_info.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-DESCRIPTION_PKG=`rospack find oberon_description`
-OBERON_DAE=$DESCRIPTION_PKG/openrave/oberon_standalone.dae
-
-openrave-robot.py $OBERON_DAE --info joints
--- uuv_manipulators/oberon/oberon_description/openrave/visualize_model.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# This script opens the OpenRAVE visualizer
-DESCRIPTION_PKG=`rospack find oberon_description`
-OBERON_DAE=$DESCRIPTION_PKG/openrave/oberon_standalone.dae
-
-openrave $OBERON_DAE
