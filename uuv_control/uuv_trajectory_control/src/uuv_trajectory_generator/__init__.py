4f95673cf5df706bbb7b18db9a3cfef5919a19f5
Musa Morena Marcusso Manhães <Musa.Marcusso@de.bosch.com>
2017-04-26 11:38:49 +0200

    ADD Trajectory generation Python module.
    Signed-off-by: Musa Morena Marcusso Manhães <Musa.Marcusso@de.bosch.com>

---
+++ uuv_control/uuv_trajectory_control/src/uuv_trajectory_generator/__init__.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2016 The UUV Simulator Authors.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .wp_trajectory_generator import WPTrajectoryGenerator
+from .trajectory_point import TrajectoryPoint
+from .waypoint import Waypoint
+from .waypoint_set import WaypointSet
+from .trajectory_generator import TrajectoryGenerator
+from .path_generator import *
