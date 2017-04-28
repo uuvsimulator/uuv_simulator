FIX Computation from max. time from the interpolated waypoint path.
Signed-off-by: Musa Morena Marcusso Manhães <Musa.Marcusso@de.bosch.com>

uuv_control/uuv_trajectory_control/src/uuv_control_interfaces/dp_controller_local_planner.py
@@ -445,22 +445,30 @@ class DPControllerLocalPlanner(object):
 
         if not self._station_keeping_on and self._traj_running:
             if self._smooth_approach_on:
+                # Generate extra waypoint before the initial waypoint
                 self._calc_smooth_approach()
                 self._smooth_approach_on = False
+            # Get interpolated reference from the reference trajectory
             self._this_ref_pnt = self._traj_interpolator.interpolate(t)
+
             if not self._traj_running:
                 self._traj_running = True
 
             if (self._this_ref_pnt is None and self._traj_running) or \
                (self._traj_running and self._traj_interpolator.has_finished()):
-               self._display_message('Trajectory completed!')
-               self._this_ref_pnt.vel = np.zeros(6)
-               self._this_ref_pnt.acc = np.zeros(6)
-               self.set_station_keeping(True)
-               self.set_automatic_mode(False)
-               self.set_trajectory_running(False)
+                # Trajectory ended, start station keeping mode
+                self._display_message('Trajectory completed!')
+                self._this_ref_pnt.vel = np.zeros(6)
+                self._this_ref_pnt.acc = np.zeros(6)
+                self.set_station_keeping(True)
+                self.set_automatic_mode(False)
+                self.set_trajectory_running(False)
         elif self._this_ref_pnt is None:
+            # Use the latest position and heading of the vehicle from the odometry to enter station keeping mode
             self._this_ref_pnt = deepcopy(self._vehicle_pose)
+            # Set roll and pitch reference to zero
+            yaw = self._this_ref_pnt.rot[2]
+            self._this_ref_pnt.rot = [0, 0, yaw]
             self.set_automatic_mode(False)
 
         return self._this_ref_pnt

uuv_control/uuv_trajectory_control/src/uuv_trajectory_generator/wp_trajectory_generator.py
@@ -234,12 +234,12 @@ class WPTrajectoryGenerator(object):
         if A == 0.0:
             return 0.0, 0.0
 
-        v = (1.0 / A) * (sx * (st * st4 - st2 * st3) + \
-                         stx * (st2 * st2 - n * st4) + \
+        v = (1.0 / A) * (sx * (st * st4 - st2 * st3) +
+                         stx * (st2 * st2 - n * st4) +
                          st2x * (n * st3 - st * st2))
 
-        a = (2.0 / A) * (sx * (st2 * st2 - st * st3) + \
-                         stx * (n * st3 - st * st2) + \
+        a = (2.0 / A) * (sx * (st2 * st2 - st * st3) +
+                         stx * (n * st3 - st * st2) +
                          st2x * (st * st - n * st2))
         return v, a
 
@@ -287,7 +287,7 @@ class WPTrajectoryGenerator(object):
         # Generate position and rotation quaternion for the current path
         # generator method
         pnt = self.interpolator.generate_pnt(
-            cur_s, cur_s * self.interpolator.max_time + self.interpolator.start_time)
+            cur_s, cur_s * (self.interpolator.max_time - self.interpolator.start_time) + self.interpolator.start_time)
         if self._use_finite_diff:
             # Set linear velocity
             pnt.vel = self._generate_vel(cur_s)
@@ -299,7 +299,7 @@ class WPTrajectoryGenerator(object):
             for ti in np.arange(pnt.t - self._regression_window / 2, pnt.t + self._regression_window, self._t_step):
                 if ti < 0:
                     si = 0
-                elif ti > self.interpolator.max_time:
+                elif ti > self.interpolator.max_time - self.interpolator.start_time:
                     si = 1
                 else:
                     si = (ti - self.interpolator.start_time) / self.interpolator.max_time