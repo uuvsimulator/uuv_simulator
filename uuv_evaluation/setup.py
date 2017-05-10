eb2c9103e6cf37ff223fd1bea5e674e90f587863
Musa Morena Marcusso Manhães <Musa.Marcusso@de.bosch.com>
2017-05-10 15:04:33 +0200

    ADD New scripts and Python modules to the catkin package description.
    Signed-off-by: Musa Morena Marcusso Manhães <Musa.Marcusso@de.bosch.com>

--- uuv_evaluation/setup.py
+++ uuv_evaluation/setup.py
@@ -17,7 +17,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['bag_evaluation', 'bag_evaluation.metrics'],
+    packages=['bag_evaluation', 'bag_evaluation.metrics', 'parameter_opt'],
     package_dir={'': 'src'},
     requires=['rospy']
 )
