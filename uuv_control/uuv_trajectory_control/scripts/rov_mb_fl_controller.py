0878d9616757fc274a259f1ec17c4f9b8cef8834
Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>
2017-10-13 17:02:59 +0200

    ADD Model-based feedback linearization controller
    Controller mostly targeted for thruster actuated robot models.
    
    Signed-off-by: Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>

---
+++ uuv_control/uuv_trajectory_control/scripts/rov_mb_fl_controller.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# Copyright (c) 2016 The UUV Simulator Authors.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rospy
+import numpy as np
+from uuv_control_interfaces import DPPIDControllerBase
+from uuv_control_msgs.srv import *
+
+class ROV_MBFLController(DPPIDControllerBase):
+    """
+    Modelbased Feedback Linearization Controller
+    Reference:
+    Thor I. Fossen 2011
+    Handbook of Marine Craft Hydrodynamics and Motion Control
+    """
+    _LABEL = 'Modelbased Feedback Linearization Controller'
+
+    def __init__(self):
+        DPPIDControllerBase.__init__(self, True)
+        self._logger.info('Initializing: Model-based feedback linearization'
+                          ' controller')
+
+        # Control forces and torques
+        self._tau = np.zeros(6)
+        # PID control vector
+        self._pid_control = np.zeros(6)
+        self._is_init = True
+        self._logger.info('Model-based feedback linearization controller ready')
+
+    def _reset_controller(self):
+        super(ROV_MBFLController, self).reset_controller()
+        self._pid_control = np.zeros(6)
+        self._tau = np.zeros(6)
+
+    def update_controller(self):
+        if not self._is_init:
+            return False
+
+        self._pid_control = self.update_pid()
+
+        vel = self._vehicle_model.to_SNAME(self._vehicle_model.vel)
+        self._vehicle_model._update_damping(vel)
+        self._vehicle_model._update_coriolis(vel)
+        self._vehicle_model._update_restoring(use_sname=True)
+
+        self._tau = np.dot(self._vehicle_model.Ctotal, vel) + \
+                    np.dot(self._vehicle_model.Dtotal, vel) + \
+                    self._vehicle_model.restoring_forces
+        # Publish control forces and torques
+        self.publish_control_wrench(self._pid_control + self._vehicle_model.from_SNAME(self._tau))
+        return True
+
+if __name__ == '__main__':
+    print('Starting Modelbased Feedback Linearization Controller')
+    rospy.init_node('rov_mb_fl_controller')
+
+    try:
+        node = ROV_MBFLController()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        print('caught exception')
+    print('exiting')
