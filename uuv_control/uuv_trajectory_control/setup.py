8a50c6f4a253bdb7f93aa9ba2b6e3dc132b7c365
Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>
2017-12-18 17:14:19 +0100

    ADD Separate Python package for waypoints package
    Signed-off-by: Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>

--- uuv_control/uuv_trajectory_control/setup.py
+++ uuv_control/uuv_trajectory_control/setup.py
@@ -7,6 +7,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages=['uuv_control_interfaces',
               'uuv_trajectory_generator',
+              'uuv_waypoints',
               'uuv_trajectory_generator.path_generator'],
     package_dir={'': 'src'},
     requires=['std_msgs', 'rospy']
