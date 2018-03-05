a7be77596239e10f13f32b4a779d4ad78b00bd95
Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>
2018-03-05 10:35:27 +0100

    FIX Gazebo 9 installation script for Travis
    Signed-off-by: Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>

--- .uuv_ci_config/install_gazebo9.sh
+++ .uuv_ci_config/install_gazebo9.sh
@@ -16,5 +16,5 @@
 
 sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
 wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
-sudo apt-get update
+sudo apt update
 sudo apt -qq install --no-install-recommends --allow-unauthenticated -y gazebo9 libgazebo9-dev ros-kinetic-gazebo9-*
