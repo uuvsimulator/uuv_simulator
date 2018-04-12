fae7f8467d873b62d27dd54e11a30db44c64b4a1
Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>
2018-04-12 23:17:36 +0200

    ADD Function to generate series of cubic Bezier segments
    Signed-off-by: Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>

--- uuv_control/uuv_trajectory_control/src/uuv_trajectory_generator/path_generator/bezier_curve.py
+++ uuv_control/uuv_trajectory_control/src/uuv_trajectory_generator/path_generator/bezier_curve.py
@@ -25,7 +25,7 @@ class BezierCurve(object):
         automatic machines and robots. Springer Science & Business Media, 2008.
     """
 
-    def __init__(self, pnts, order, tangents=None):
+    def __init__(self, pnts, order, tangents=None, normals=None):
         assert order in [3, 4, 5], 'Invalid Bezier curve order'
         assert type(pnts) == list and len(pnts) >= 2, 'At least two points are needed to calculate the curve'
 
@@ -89,27 +89,132 @@ class BezierCurve(object):
             self._control_pnts[1] = self._control_pnts[0] + 0.25 * alpha * tangents[0]
             self._control_pnts[3] = self._control_pnts[4] - 0.25 * alpha * tangents[1]
         elif self._order == 5:
-            assert len(self._pnts) == 3, 'Three points are needed for the curve to be computed'
-            # Setting initial control points
-            self._control_pnts[0] = self._pnts[0]
-            self._control_pnts[5] = self._pnts[2]
+            if len(self._pnts) == 3:            
+                # Setting initial control points
+                self._control_pnts[0] = self._pnts[0]
+                self._control_pnts[5] = self._pnts[2]
 
-            radius = np.linalg.norm(self._pnts[0] - self._pnts[1])
-            tangents = list()
-            tangents.append((self._pnts[1] - self._pnts[0]) / radius)
-            tangents.append((self._pnts[2] - self._pnts[1]) / radius)
+                radius = np.linalg.norm(self._pnts[0] - self._pnts[1])
+                tangents = list()
+                tangents.append((self._pnts[1] - self._pnts[0]) / radius)
+                tangents.append((self._pnts[2] - self._pnts[1]) / radius)
 
-            # Compute alpha
-            a = 256 - 49 * np.linalg.norm(tangents[0] + tangents[1])**2
-            b = 420 * np.dot(self._control_pnts[5] - self._control_pnts[0], tangents[0] + tangents[1])
-            c = -900 * np.linalg.norm(self._control_pnts[5] - self._control_pnts[0])**2
-            alpha = np.roots([a, b, c]).max()
+                # Compute alpha
+                a = 256 - 49 * np.linalg.norm(tangents[0] + tangents[1])**2
+                b = 420 * np.dot(self._control_pnts[5] - self._control_pnts[0], tangents[0] + tangents[1])
+                c = -900 * np.linalg.norm(self._control_pnts[5] - self._control_pnts[0])**2
+                alpha = np.roots([a, b, c]).max()
 
-            # Compute the rest of the control points
-            self._control_pnts[1] = self._control_pnts[0] + 0.2 * alpha * tangents[0]
-            self._control_pnts[2] = 2 * self._control_pnts[1] - self._control_pnts[0]
-            self._control_pnts[4] = self._control_pnts[5] - 0.2 * alpha * tangents[1]
-            self._control_pnts[3] = 2 * self._control_pnts[4] - self._control_pnts[5]
+                # Compute the rest of the control points
+                self._control_pnts[1] = self._control_pnts[0] + 0.2 * alpha * tangents[0]
+                self._control_pnts[2] = 2 * self._control_pnts[1] - self._control_pnts[0]
+                self._control_pnts[4] = self._control_pnts[5] - 0.2 * alpha * tangents[1]
+                self._control_pnts[3] = 2 * self._control_pnts[4] - self._control_pnts[5]
+            elif len(self._pnts) == 2:                
+                assert tangents is not None and normals is not None
+                assert isinstance(tangents, list) and len(tangents) == 2, 'Tangent vectors must be provided'
+                assert isinstance(normals, list) and len(normals) == 2, 'Normal vectors must be provided'
+
+                beta_hat = 0.51
+                                
+                a = beta_hat**2 * np.linalg.norm(normals[1] - normals[0])**2
+                b = -28 * beta_hat * np.dot((tangents[0] + tangents[1]), normals[1] - normals[0])
+                c = 196 * np.linalg.norm(tangents[0] + tangents[1])**2 + 120 * beta_hat * np.dot(self._pnts[1] - self._pnts[0], normals[1] - normals[0]) - 1024
+                d = -1680 * np.dot(self._pnts[1] - self._pnts[0], tangents[0] + tangents[1])
+                e = 3600 * np.linalg.norm(self._pnts[1] - self._pnts[0])**2
+                    
+                alpha_k = np.real(np.roots([a, b, c, d, e])).max()
+
+                # Setting initial control points
+                self._control_pnts[0] = self._pnts[0]
+                self._control_pnts[5] = self._pnts[1]
+
+                self._control_pnts[1] = self._control_pnts[0] + alpha_k / 5.0 * tangents[0]
+                self._control_pnts[2] = 2.0 * self._control_pnts[1] - self._control_pnts[0] + beta_hat * alpha_k**2 / 20.0 * normals[0]
+                self._control_pnts[4] = self._control_pnts[5] - alpha_k / 5.0 * tangents[1]
+                self._control_pnts[3] = 2.0 * self._control_pnts[4] - self._control_pnts[5] + beta_hat * alpha_k**2 / 20.0 * normals[1]
+
+    @staticmethod
+    def distance(p1, p2):
+        p1 = np.array(p1)
+        p2 = np.array(p2)
+
+        assert p1.size == 3 and p2.size == 3        
+        return np.sqrt(np.sum((p2 - p1)**2))
+
+    @staticmethod
+    def generate_cubic_curve(pnts):
+        assert isinstance(pnts, list), 'List of points is invalid'
+        tangents = [np.zeros(3) for _ in range(len(pnts))]
+
+        lengths = [BezierCurve.distance(pnts[i + 1], pnts[i]) for i in range(len(pnts) - 1)]
+        lengths = [0] + lengths
+        
+        # Initial vector of parametric variables for the curve
+        u = [l / np.sum(lengths) for l in np.cumsum(lengths)]
+        delta_u = lambda k: u[k] - u[k - 1]
+        delta_q = lambda k: pnts[k] - pnts[k - 1]
+        lamb_k = lambda k: delta_q(k) / delta_u(k)
+        alpha_k = lambda k: delta_u(k) / (delta_u(k) + delta_u(k + 1))
+
+        for i in range(1, len(u) - 1):            
+            tangents[i] = (1 - alpha_k(i)) * lamb_k(i) + alpha_k(i) * lamb_k(i + 1)
+            if i == 1:
+                tangents[0] = 2 * lamb_k(i) - tangents[1]
+
+        tangents[-1] = 2 * lamb_k(len(u) - 1) - tangents[-2]
+
+        # Normalize tangent vectors
+        for i in range(len(tangents)):
+            tangents[i] = tangents[i] / np.linalg.norm(tangents[i])
+
+        segments = list()
+        # Generate the cubic Bezier curve segments
+        for i in range(len(tangents) - 1):
+            segments.append(BezierCurve([pnts[i], pnts[i + 1]], 3, [tangents[i], tangents[i + 1]]))
+
+        return segments
+
+    @staticmethod
+    def generate_quintic_curve(pnts):
+        assert isinstance(pnts, list), 'List of points is invalid'
+        tangents = [np.zeros(3) for _ in range(len(pnts))]
+        normals = [np.zeros(3) for _ in range(len(pnts))]
+
+        lengths = [BezierCurve.distance(pnts[i + 1], pnts[i]) for i in range(len(pnts) - 1)]
+        lengths = [0] + lengths
+        # Initial vector of parametric variables for the curve
+        u = np.cumsum(lengths) / np.sum(lengths)                
+        
+        delta_u = lambda k: u[k] - u[k - 1]
+        delta_q = lambda k: pnts[k] - pnts[k - 1]
+        lamb_k = lambda k: delta_q(k) / delta_u(k)
+        alpha_k = lambda k: delta_u(k) / (delta_u(k) + delta_u(k + 1))
+        normal_k = lambda k: ( ((pnts[k + 1] - pnts[k]) / (u[k + 1] - u[k])) - ((pnts[k] - pnts[k - 1]) / (u[k] - u[k - 1])) ) / (u[k + 1] - u[k - 1])
+
+        for i in range(1, len(u) - 1):
+            tangents[i] = (1 - alpha_k(i)) * lamb_k(i) + alpha_k(i) * lamb_k(i + 1)
+            normals[i] = normal_k(i)
+            if i == 1:
+                tangents[0] = 2 * lamb_k(i) - tangents[1]
+                normals[0] = normal_k(i)
+
+        tangents[-1] = 2 * lamb_k(len(u) - 1) - tangents[-2]
+        normals[-1] = normal_k(len(u) - 3)
+                
+        # Normalize tangent vectors
+        for i in range(len(tangents)):
+            tangents[i] /= np.linalg.norm(tangents[i])
+            normals[i] /= np.linalg.norm(normals[i])
+
+        segments = list()
+        # Generate the cubic Bezier curve segments
+        for i in range(len(tangents) - 1):
+            segments.append(BezierCurve([pnts[i], pnts[i + 1]], 5, 
+                [tangents[i], tangents[i + 1]], 
+                [normals[i], normals[i + 1]]))
+
+        return segments
 
     def control_pnts(self):
         return self._control_pnts
@@ -123,13 +228,13 @@ class BezierCurve(object):
             b += self.compute_polynomial(self._order, i, u) * self._control_pnts[i]
         return b
 
-    def get_derivative(self, u):
+    def get_derivative(self, u, order=1):
         u = max(u, 0)
         u = min(u, 1)
 
         b = np.zeros(3)        
-        for i in range(len(self._control_pnts) - 1):
-            b = b + self._order * self.compute_polynomial(self._order - 1, i, u) * \
+        for i in range(len(self._control_pnts) - order):
+            b = b + self._order * self.compute_polynomial(self._order - order, i, u) * \
                  (self._control_pnts[i + 1] - self._control_pnts[i])        
         return b
 
@@ -147,51 +252,28 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     from mpl_toolkits.mplot3d import Axes3D
 
+    np.set_printoptions(suppress=True, precision=2)
+
     print 'Test - Cubic Bezier curve'
     q_x = [0, 1, 2, 4, 5, 6]
     q_y = [0, 2, 3, 3, 2, 0]
     q_z = [0, 1, 0, 0, 2, 2]
 
-    q = np.vstack((q_x, q_y, q_z)).T
-
+    q = [np.array([x, y, z]) for x, y, z in zip(q_x, q_y, q_z)]
+    
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    ax.plot(q[:, 0], q[:, 1], q[:, 2], 'b--')
-    ax.plot(q[:, 0], q[:, 1], q[:, 2], 'ro')
+    ax.plot(q_x, q_y, q_z, 'b--')
+    ax.plot(q_x, q_y, q_z, 'ro')
 
-    # Compute the distance between points
-    lengths = [np.linalg.norm(q[i + 1, :] - q[i, :]) for i in range(q.shape[0] - 1)]
-    lengths = [0] + lengths
-    u = np.cumsum(lengths) / np.sum(lengths)
-    print q.T
-    print 'u=', u
+    # Compute cubic Bezier curve
 
-    tangents = [np.zeros(3) for _ in range(q.shape[0])]
-    delta_u = lambda k: u[k] - u[k - 1]
-    delta_q = lambda k: q[k, :] - q[k - 1, :]
-    lamb_k = lambda k: delta_q(k) / delta_u(k)
-    alpha_k = lambda k: delta_u(k) / (delta_u(k) + delta_u(k + 1))
-    for i in range(1, len(u) - 1):
-        tangents[i] = (1 - alpha_k(i)) * lamb_k(i) + alpha_k(i) * lamb_k(i + 1)
-        if i == 1:
-            tangents[0] = 2 * lamb_k(i) - tangents[1]
+    segments, tangents = BezierCurve.generate_cubic_curve(q)
 
-    tangents[-1] = 2 * lamb_k(len(u) - 1) - tangents[-2]
-
-    print 'Tangents'
-    for i in range(len(tangents)):
-        tangents[i] = tangents[i] / np.linalg.norm(tangents[i])
-        print '\t#%d - %.2f %.2f %.2f' % (i, tangents[i][0], tangents[i][1], tangents[i][2])
-        ax.plot([q[i, 0], tangents[i][0] + q[i, 0]],
-                [q[i, 1], tangents[i][1] + q[i, 1]],
-                [q[i, 2], tangents[i][2] + q[i, 2]], 'c')
-
-    segments = list()
-    print 'Segments'
-    for i in range(len(tangents) - 1):
-        segments.append(BezierCurve([q[i, :], q[i + 1, :]], 3, tangents[i:i + 2]))
-        print '\t', segments[-1]._control_pnts
+    for i in range(len(q)):
+        t = 0.8 * tangents[i]
+        ax.plot([q[i][0], q[i][0] + t[0]], [q[i][1], q[i][1] + t[1]], [q[i][2], q[i][2] + t[2]], 'b', linewidth=3)
 
     lengths = [seg.get_length() for seg in segments]
     lengths = [0] + lengths
@@ -201,7 +283,7 @@ if __name__ == '__main__':
 
     pnts = None
     deriv = None
-    for i in np.linspace(0, 1, 100):
+    for i in np.linspace(0, 1, 200):
         idx = (u - i >= 0).nonzero()[0][0]
         if idx == 0:
             u_k = 0
@@ -212,34 +294,67 @@ if __name__ == '__main__':
             pnts = np.vstack((pnts, segments[idx - 1].interpolate(u_k)))
             deriv = np.vstack((deriv, segments[idx - 1].get_derivative(u_k)))
 
-    ax.plot(pnts[:, 0], pnts[:, 1], pnts[:, 2], 'g')
+    ax.plot(pnts[:, 0], pnts[:, 1], pnts[:, 2], 'g')    
+
+    for d, p in zip(deriv, pnts):
+        d /= np.linalg.norm(d) 
+        pd = p + d 
+        # ax.plot([p[0], pd[0]], [p[1], pd[1]], [p[2], pd[2]], 'r')
+
+    ax.set_aspect('equal')
+
+    u = np.array([0, 0.22, 0.38, 0.56, 0.79, 1])
+
+    for i in u:
+        idx = (u - i >= 0).nonzero()[0][0]
+        if idx == 0:
+            u_k = 0            
+            deriv = segments[idx].get_derivative(u_k)
+        else:
+            u_k = (i - u[idx - 1]) / (u[idx] - u[idx - 1])            
+            deriv = np.vstack((deriv, segments[idx - 1].get_derivative(u_k)))
+        
+    # Compute Quintic Bezier curve    
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+
+    # ax.plot(q_x, q_y, q_z, 'b--')
+    # ax.plot(q_x, q_y, q_z, 'ro')
+
+    # segments, tangents, normals = BezierCurve.generate_quintic_curve(q)
+
+    # for i in range(len(q)):
+    #     t = tangents[i]
+    #     ax.plot([q[i][0], q[i][0] + t[0]], [q[i][1], q[i][1] + t[1]], [q[i][2], q[i][2] + t[2]], 'b', linewidth=3)
+
+    #     n = normals[i]
+    #     ax.plot([q[i][0], q[i][0] + n[0]], [q[i][1], q[i][1] + n[1]], [q[i][2], q[i][2] + n[2]], 'r', linewidth=3)
+
+    # lengths = [seg.get_length() for seg in segments]
+    # lengths = [0] + lengths
+    # total_length = np.sum(lengths)
+
+    # u = np.cumsum(lengths) / total_length
+
+    # pnts = None
+    # deriv = None
+    # for i in np.linspace(0, 1, 100):
+    #     idx = (u - i >= 0).nonzero()[0][0]
+    #     if idx == 0:
+    #         u_k = 0
+    #         pnts = segments[idx].interpolate(u_k)
+    #         deriv = segments[idx].get_derivative(u_k)
+    #     else:
+    #         u_k = (i - u[idx - 1]) / (u[idx] - u[idx - 1])
+    #         pnts = np.vstack((pnts, segments[idx - 1].interpolate(u_k)))
+    #         deriv = np.vstack((deriv, segments[idx - 1].get_derivative(u_k)))        
 
     # for d, p in zip(deriv, pnts):
     #     d /= np.linalg.norm(d) 
-    #     pd = p + d * 0.001
-    #     ax.plot([p[0], p[0] + pd[0]], [p[1], p[1] + pd[1]], [p[2], p[2] + pd[2]], 'r')
+    #     pd = p + d 
+        # ax.plot([p[0], pd[0]], [p[1], pd[1]], [p[2], pd[2]], 'r')
 
-    fig = plt.figure()
-
-    u_hat = np.linspace(0, 1, 100)
-    d_x = [(pnts[i, 0] - pnts[i - 1, 0]) / (u_hat[i] - u_hat[i - 1]) for i in range(1, pnts.shape[0])]
-    d_y = [(pnts[i, 1] - pnts[i - 1, 1]) / (u_hat[i] - u_hat[i - 1]) for i in range(1, pnts.shape[0])]
-    d_z = [(pnts[i, 2] - pnts[i - 1, 2]) / (u_hat[i] - u_hat[i - 1]) for i in range(1, pnts.shape[0])]
-
-    ax = fig.add_subplot(311)
-    ax.plot(u_hat[1::], d_x)
-    ax.set_xlim([u_hat[1::].min(), u_hat[1::].max()])
-    ax.grid(True)
-
-    ax = fig.add_subplot(312)
-    ax.plot(u_hat[1::], d_y)
-    ax.set_xlim([u_hat[1::].min(), u_hat[1::].max()])
-    ax.grid(True)
-
-    ax = fig.add_subplot(313)
-    ax.plot(u_hat[1::], d_z)
-    ax.set_xlim([u_hat[1::].min(), u_hat[1::].max()])
-    ax.grid(True)
-
+    # ax.plot(pnts[:, 0], pnts[:, 1], pnts[:, 2], 'c')
+    
     plt.show()
 
