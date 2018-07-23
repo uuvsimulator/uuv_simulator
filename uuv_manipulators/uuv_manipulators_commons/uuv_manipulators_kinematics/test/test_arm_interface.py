9aae559c79ee7b87e943e881b7ea3bf51eb8f701
Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>
2018-07-23 11:02:09 +0200

    RM Forward/inverse kinematics tests for now
    Signed-off-by: Musa Morena Marcusso Manhaes <Musa.Marcusso@de.bosch.com>

--- uuv_manipulators/uuv_manipulators_commons/uuv_manipulators_kinematics/test/test_arm_interface.py
+++ uuv_manipulators/uuv_manipulators_commons/uuv_manipulators_kinematics/test/test_arm_interface.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+from __future__ import print_function
 PKG = 'uuv_manipulators_kinematics'
 import roslib; roslib.load_manifest(PKG)
 
@@ -22,20 +22,17 @@ import unittest
 import numpy as np
 from uuv_manipulator_interfaces import ArmInterface
 
-HOME_CART_POS = np.array([0.45318439, 0.00000000, 0.13526251])
-
-HOME_CART_ROT = np.array([0.00000000, -0.05232793, 0.00000000, 0.99862996])
 
 class TestArmInterface(unittest.TestCase):
     def test_init_interface(self):
          arm = ArmInterface()
          # Test if the namespace and arm name are correct
          self.assertEquals(arm.namespace, '/rexrov/', 'Invalid robot namespace')
-         self.assertEquals(arm.arm_name, 'oberon', 'Invalid arm name')
-         self.assertEquals(arm.base_link, 'oberon/base', 'Invalid manipulator base name')
-         self.assertEquals(arm.tip_link, 'oberon/end_effector', 'Invalid end-effector link name')
+         self.assertEquals(arm.arm_name, 'oberon7', 'Invalid arm name')
+         self.assertEquals(arm.base_link, 'oberon7/base', 'Invalid manipulator base name')
+         self.assertEquals(arm.tip_link, 'oberon7/end_effector', 'Invalid end-effector link name')
          self.assertNotEquals(len(arm.joint_names), 0, 'The list of joint names is empty')
-         self.assertEquals(arm.n_links, 7, 'Invalid number of links')
+         self.assertEquals(arm.n_links, 6, 'Invalid number of links, n_links=' + str(arm.n_links))
 
          for name in arm.joint_names:
              self.assertIn(name, arm.joint_angles, 'Joint name %s not listed in the joint positions dictionary' % name)
@@ -54,29 +51,16 @@ class TestArmInterface(unittest.TestCase):
         arm = ArmInterface()
         jac = arm.jacobian()
         self.assertIsNotNone(jac, 'Jacobian matrix is invalid')
-        self.assertEquals(jac.shape, (arm.n_links - 1, 6), 'The full Jacobian matrix has the wrong size')
-        for idx, name in zip(range(len(arm.joint_names)), arm.joint_names):
-            self.assertEquals(arm.jacobian(last_joint=name).shape, (arm.n_links - 1, idx + 1))
-            self.assertEquals(arm.jacobian_transpose(last_joint=name).shape, (idx + 1, arm.n_links - 1))
+        self.assertEquals(jac.shape, (arm.n_links, 6), 'The full Jacobian matrix has the wrong size')
+
+        for idx, name in zip(range(len(arm.link_names)), arm.link_names):
+            self.assertEquals(arm.jacobian(end_link=name).shape, (arm.n_links, 6))
+            self.assertEquals(arm.jacobian_transpose(end_link=name).shape, (6, arm.n_links))
 
     def test_home_config(self):
         arm = ArmInterface()
         self.assertIsNotNone(arm.home, 'Home configuration is invalid')
 
-    def test_forward_position_kinematics(self):
-        arm = ArmInterface()
-        jnt_array = arm.home
-        pose = arm.forward_position_kinematics(arm.home)
-
-        self.assertLess(np.abs(pose[0:3] - HOME_CART_POS).sum(), 1e-6, 'Invalid output position, pos=[%.8f, %.8f, %.8f]' % (pose[0], pose[1], pose[2]))
-        self.assertLess(np.abs(pose[3::] - HOME_CART_ROT).sum(), 1e-6, 'Invalid output orientation, rot=[%.8f, %.8f, %.8f, %.8f]' % (pose[3], pose[4], pose[5], pose[6]))
-
-    def test_inv_kinematics(self):
-        arm = ArmInterface()
-        home_q = arm.home
-        q = arm.inverse_kinematics(HOME_CART_POS, HOME_CART_ROT)
-        for idx in range(len(arm.joint_names)):
-            self.assertLess(np.abs(home_q[arm.joint_names[idx]] - q[idx]), 1e-6, 'home_q=%.4f, q=%.4f' % (home_q[arm.joint_names[idx]], q[idx]))
 
 if __name__ == '__main__':
     import rosunit
