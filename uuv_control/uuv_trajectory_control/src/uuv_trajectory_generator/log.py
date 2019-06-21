319ac92539d1e1de95365e83a95b2f789b2c9071
Musa Morena Marcusso Manhães <musa.marcusso@de.bosch.com>
2019-06-21 23:24:37 +0200

    Add local logging module
    Signed-off-by: Musa Morena Marcusso Manhães <musa.marcusso@de.bosch.com>

---
+++ uuv_control/uuv_trajectory_control/src/uuv_trajectory_generator/log.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2016-2019 The UUV Simulator Authors.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOGGER = None
+
+def get_logger():
+    global LOGGER
+    if LOGGER is None:
+        import logging
+        import sys
+        
+        LOGGER = logging.getLogger('uuv_trajectory_generator')
+        out_hdlr = logging.StreamHandler(sys.stdout)
+        out_hdlr.setFormatter(logging.Formatter(' %(asctime)s | %(levelname)s | %(module)s | %(message)s'))
+        out_hdlr.setLevel(logging.INFO)
+        LOGGER.addHandler(out_hdlr)
+        LOGGER.setLevel(logging.INFO)
+
+    return LOGGER
