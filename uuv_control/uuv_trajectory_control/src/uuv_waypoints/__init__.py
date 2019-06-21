6991c15a16a2485c1bba1a8ce55236e8871cd71d
Musa Morena Marcusso Manhães <musa.marcusso@de.bosch.com>
2019-06-21 23:25:26 +0200

    Fix module docstring breakline
    Signed-off-by: Musa Morena Marcusso Manhães <musa.marcusso@de.bosch.com>

--- uuv_control/uuv_trajectory_control/src/uuv_waypoints/__init__.py
+++ uuv_control/uuv_trajectory_control/src/uuv_waypoints/__init__.py
@@ -12,8 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Waypoint description for construction of 3D paths and trajectories.
-"""
+"""Waypoint description for construction of 3D paths and trajectories."""
+
 __all__ = ['waypoint', 'waypoint_set']
 
 from .waypoint import Waypoint
