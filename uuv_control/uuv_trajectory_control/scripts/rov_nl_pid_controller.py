79fa8bbb5f9ba37930e6ac4983d5c8035d22191d
Musa Morena Marcusso Manhães <musa.marcusso@de.bosch.com>
2019-06-30 14:36:58 +0200

    Update license headers
    Signed-off-by: Musa Morena Marcusso Manhães <musa.marcusso@de.bosch.com>

--- uuv_control/uuv_trajectory_control/scripts/rov_nl_pid_controller.py
+++ uuv_control/uuv_trajectory_control/scripts/rov_nl_pid_controller.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2016 The UUV Simulator Authors.
+# Copyright (c) 2016-2019 The UUV Simulator Authors.
 # All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import rospy
 import numpy as np
 from uuv_control_interfaces import DPPIDControllerBase
--- uuv_control/uuv_trajectory_control/scripts/rov_ua_pid_controller.py
+++ uuv_control/uuv_trajectory_control/scripts/rov_ua_pid_controller.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2014 The UUV Simulator Authors.
+# Copyright (c) 2016-2019 The UUV Simulator Authors.
 # All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import numpy as np
 import rospy
 from uuv_control_msgs.srv import *
