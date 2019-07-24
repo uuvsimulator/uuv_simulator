8a84e71a692be193076309726ed6318546867492
Musa Morena Marcusso Manhães <musa.marcusso@de.bosch.com>
2019-07-24 23:46:43 +0200

    Update license header
    Signed-off-by: Musa Morena Marcusso Manhães <musa.marcusso@de.bosch.com>

--- uuv_control/uuv_auv_control_allocator/src/uuv_auv_actuator_interface/fin_model.py
+++ uuv_control/uuv_auv_control_allocator/src/uuv_auv_actuator_interface/fin_model.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 The UUV Simulator Authors.
+# Copyright (c) 2016-2019 The UUV Simulator Authors.
 # All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import rospy
 import numpy as np
 from tf_quaternion.transformations import quaternion_matrix
