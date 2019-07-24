89acb60e0f1e0353d61928d3978c34bcf0213889
Musa Morena Marcusso Manhães <musa.marcusso@de.bosch.com>
2019-07-24 23:49:42 +0200

    Update license header and add docstrings
    Signed-off-by: Musa Morena Marcusso Manhães <musa.marcusso@de.bosch.com>

--- uuv_control/uuv_thruster_manager/src/uuv_thrusters/models/thruster.py
+++ uuv_control/uuv_thruster_manager/src/uuv_thrusters/models/thruster.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 The UUV Simulator Authors.
+# Copyright (c) 2016-2019 The UUV Simulator Authors.
 # All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,36 +12,32 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import rospy
 import numpy
-import tf.transformations as trans
+from tf_quaternion import transformations
 from uuv_gazebo_ros_plugins_msgs.msg import FloatStamped
 from std_msgs.msg import Header
 
 
 class Thruster(object):
-    """Abstract function to all the thruster models avaialble. The instance
-    of a thruster model must use the factory method."""
-
+    """Abstract function to all the thruster models avaialble. 
+    The instance of a thruster model must use the factory method.
+    
+    > *Input arguments*
+    
+    * `index` (*type:* `int`): Thruster's ID.
+    * `topic` (*type:* `str`): Thruster's command topic.
+    * `pos` (*type:* `numpy.array` or `list`): Position vector 
+    of the thruster with respect to the vehicle's frame.
+    * `orientation` (*type:* `numpy.array` or `list`): Quaternion 
+    with the orientation of the thruster with respect to the vehicle's
+    frame as `(qx, qy, qz, qw)`.
+    * `axis` (*type:* `numpy.array`): Axis of rotation of the thruster.
+    """
     LABEL = ''
     DEFAULT_AXIS=numpy.array([1, 0, 0, 0])
 
     def __init__(self, index, topic, pos, orientation, axis=DEFAULT_AXIS):
-        """Thruster class constructor.
-
-        Parameters
-        ----------
-        index: int
-            Thruster's index.
-        topic: str
-            Name of the thruster's command topic.
-        pos: numpy.array
-            3D position of the thruster with relation to the reference frame.
-        orientation: numpy.array
-            Quaternion with the orientation of the thruster with relation to
-            the reference frame.
-        """
         self._index = index
         self._topic = topic
         self._pos = None
@@ -51,7 +47,7 @@ class Thruster(object):
             self._pos = pos
             self._orientation = orientation
             # compute contribution to configuration matrix of this thruster
-            thrust_body = trans.quaternion_matrix(orientation).dot(
+            thrust_body = transformations.quaternion_matrix(orientation).dot(
                 axis.transpose())[0:3]
             torque_body = numpy.cross(pos, thrust_body)
             self._force_dist = numpy.hstack((
@@ -61,7 +57,16 @@ class Thruster(object):
         self._command_pub = rospy.Publisher(self._topic, FloatStamped,
                                             queue_size=10)
 
-        print 'Thruster #%d - %s - %s' % (self._index, self.LABEL, self._topic)
+        rospy.loginfo('Thruster #{} - {} - {}'.format(
+            self._index, self.LABEL, self._topic))
+
+    @property
+    def index(self):
+        return self._index
+
+    @property
+    def topic(self):
+        return self._topic
 
     @property
     def tam_column(self):
@@ -69,19 +74,30 @@ class Thruster(object):
 
     @staticmethod
     def create_thruster(model_name, *args, **kwargs):
+        """Factory method for the thruster models.
+        
+        > *Input arguments*
+        
+        * `model_name` (*type:* `str`): Name identifier of
+        the thruster model.
+        
+        > *Returns*
+        
+        Thruster model instance.
+        """
         for thruster in Thruster.__subclasses__():
             if model_name == thruster.LABEL:
                 return thruster(*args, **kwargs)
-        rospy.ROSException('Invalid thruster model')
+        raise rospy.ROSException('Invalid thruster model')
 
     def get_command_value(self, thrust):
         """Convert desired thrust force to input command according to this
         function. Overwrite this method to implement custom models."""
-        return 0
+        raise NotImplementedError()
 
     def get_thrust_value(self, command):
         """Computes the thrust force for the given command."""
-        return 0
+        raise NotImplementedError()
 
     def get_curve(self, min_value, max_value, n_points):
         """Sample the conversion curve and return the values."""
@@ -94,8 +110,8 @@ class Thruster(object):
         return input_values.tolist(), output_values
 
     def _calc_command(self):
-        """Convert the desired thrust force into angular velocity command
-        according using a gain."""
+        """Convert the desired thrust force into angular velocity 
+        command according using a gain."""
         self._command = self.get_command_value(self._thrust)
 
     def _update(self, thrust):
@@ -103,6 +119,14 @@ class Thruster(object):
         self._calc_command()
 
     def publish_command(self, thrust):
+        """Publish the thrust force command set-point
+        to a thruster unit.
+        
+        > *Input arguments*
+        
+        * `thrust` (*type:* `float`): Thrust force set-point
+        in N.
+        """
         self._update(thrust)
         output = FloatStamped()
         output.header.stamp = rospy.Time.now()
