ec7c1d4624b1b750f63cceebec2e1c65d82b4658
XY <ng.xingyu@u.nus.edu>
2021-10-23 13:20:15 +0800

    Changed python dependencies to python3 Add missing return statements

--- uuv_tutorials/uuv_tutorial_dp_controller/scripts/tutorial_dp_controller.py
+++ uuv_tutorials/uuv_tutorial_dp_controller/scripts/tutorial_dp_controller.py
@@ -90,7 +90,7 @@ class TutorialDPController(DPControllerBase):
             diag = rospy.get_param('~Kd')
             if len(diag) == 6:
                 self._Kd = np.diag(diag)
-                print 'Kd=\n', self._Kd
+                print('Kd=\n', self._Kd)
             else:
                 # If the vector provided has the wrong dimension, raise an exception
                 raise rospy.ROSException('For the Kd diagonal matrix, 6 coefficients are needed')
@@ -99,7 +99,7 @@ class TutorialDPController(DPControllerBase):
             diag = rospy.get_param('~Ki')
             if len(diag) == 6:
                 self._Ki = np.diag(diag)
-                print 'Ki=\n', self._Ki
+                print('Ki=\n', self._Ki)
             else:
                 # If the vector provided has the wrong dimension, raise an exception
                 raise rospy.ROSException('For the Ki diagonal matrix, 6 coefficients are needed')
